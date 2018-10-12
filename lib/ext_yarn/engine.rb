d33ed612f425561b9c884ef3f214dd515c86ff56
Patrice Lebel <patleb@users.noreply.github.com>
2018-10-12 06:50:46 -0400

    moved node_modules into vendor directory (Rubymine wasn't working well with it)

--- lib/ext_yarn/engine.rb
+++ lib/ext_yarn/engine.rb
@@ -1,14 +1,14 @@
 module ExtYarn
   class Engine < ::Rails::Engine
     initializer 'ext_yarn.assets.paths' do |app|
-      app.config.assets.paths << root.join('node_modules')
+      app.config.assets.paths << root.join('vendor')
 
       %w(javascripts stylesheets).each do |type|
-        app.config.assets.paths << root.join('node_modules/bootstrap-sass/assets', type)
+        app.config.assets.paths << root.join('vendor/bootstrap-sass/assets', type)
       end
 
       %w(fonts stylesheets).each do |type|
-        app.config.assets.paths << root.join('node_modules/font-awesome-sass/assets', type)
+        app.config.assets.paths << root.join('vendor/font-awesome-sass/assets', type)
       end
     end
   end
